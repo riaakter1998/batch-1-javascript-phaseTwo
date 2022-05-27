46a92362f93ab48aef36d166c3d983b360f36df2
riaakter1998 <ria426306@gmail.com>
2022-05-27 06:43:49 -0600

    class-6

---
+++ class 6/app.js
@@ -0,0 +1,13 @@
+
+// Object
+
+let ria ={
+    name : "ria akter",
+    adress : "bogura sadar",
+    distric:"bogura",
+    phonenumber : 01741370512,
+    country: "bangladesh"
+
+}
+document.write(ria.name,", ",ria.adress);
+
